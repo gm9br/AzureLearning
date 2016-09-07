202f8b8d0df3432dcc4b0b6c22c2b2a594eaf169
Gustavo Zimmermann <gustavo_percio@hotmail.com>
2016-09-07 12:06:49 -0300

    Initial commit

---
+++ AzureWebsiteAspNetCoreFramework/src/AzureWebsiteAspNetCoreFramework/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AzureWebsiteAspNetCoreFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            Trace.TraceError("This is an error message from About");
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            Trace.TraceError("This is an error message from Contact");
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
